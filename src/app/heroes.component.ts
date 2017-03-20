5b8f5b6e63db57aab0b918a6b0ca55a8b1eef43f
Igor <ig0r260892@gmail.com>
2017-03-20 02:06:53 +0000

    Import and declare HeroesComponent in app.module
    Move HeroService from within hero.component to app.module's providers array.

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -9,7 +9,6 @@ import { HeroService } from './hero.service';
 @Component({
 	selector: 'my-heroes',
 	template: `
-		<h1>{{title}}</h1>
 		<h2>My Heroes</h2>
 		<ul class="heroes">
 			<li *ngFor="let hero of heroes"
@@ -70,11 +69,9 @@ import { HeroService } from './hero.service';
 			margin-right: .8em;
 			border-radius: 4px 0 0 4px;
 		}
-	`],
-	providers: [HeroService]
+	`]
 })
 export class HeroesComponent implements OnInit {
-	title = 'Tour of Heroes';
 	heroes: Hero[];
 	selectedHero: Hero;
 
